10a9e78e44570a69ec5cfe7af0dce09a42de9dcf
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2021-12-24 03:18:02 +0700

    Added random ping pong gifs.

--- customping/ping.py
+++ customping/ping.py
@@ -21,6 +21,7 @@ SOFTWARE.
 # Remove command logic originally from: https://github.com/mikeshardmind/SinbadCogs/tree/v3/messagebox
 # Speed test logic from https://github.com/PhasecoreX/PCXCogs/tree/master/netspeed
 
+from random import choice
 import asyncio
 import concurrent
 import datetime
@@ -84,8 +85,22 @@ class CustomPing(commands.Cog):
         message = await ctx.send("Pinging...")
         end = time.monotonic()
         totalPing = round((end - start) * 1000, 2)
+        ping_gifs = (
+            "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
+            "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
+            "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
+            "https://i.imgur.com/LkdjWE6.gif",
+            "https://i.gifer.com/6TaL.gif",
+            "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
+            "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
+            "https://imgur.com/1cnscjV.gif",
+            "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
+            "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
+        )
+        ping_gifs_picker = random.choice(ping_gifs)
         e = discord.Embed(title="Pinging..")
         e.add_field(name="Overall:", value=chat.box(f"{totalPing}" + "ms", "crmsh"))
+        e.set_image(url=ping_gifs_picker)
         await asyncio.sleep(0.25)
         try:
             await message.edit(content=None, embed=e)
