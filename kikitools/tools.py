636487a40896e55edd22cc46ce69417a910d16bd
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2021-12-27 14:38:02 +0700

    Added [p]vote

--- kikitools/tools.py
+++ kikitools/tools.py
@@ -225,7 +225,7 @@ class KikiTools(commands.Cog):
     @commands.bot_has_permissions(embed_links=True)
     async def vote(self, ctx):
         """Vote for me!"""
-        c = await self.bot.get_embed_colour(message.channel)
+        c = await self.bot.get_embed_colour()
         t = "Vote for Me!"
         f = "Thanks for your support!"
         i = await self.config.current_avatar()
