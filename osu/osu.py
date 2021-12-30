00716ac90ca4ea59c4f5761aac01d10122727738
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2021-12-31 02:14:58 +0700

    [Osu] Fixed new lines.

--- osu/osu.py
+++ osu/osu.py
@@ -44,8 +44,8 @@ class Osu(BaseCog):
             pp = "**▸ PP:** " + osu[0]["pp_raw"] + "\n"
             acc = "**▸ Accuracy:** " + osu[0]["accuracy"][:6] + "%\n"
             ranks = "**▸ Ranks:** " + f"{SSH}" + "`" + osu[0]["count_rank_ssh"] + "`" + f"{SS}" + "`" + osu[0]["count_rank_ss"] + "`" + f"{SH}" + "`" + osu[0]["count_rank_sh"] + "`" + f"{S}" + "`" + osu[0]["count_rank_s"] + "`" + f"{A}" + "`" + osu[0]["count_rank_a"] + "`" + "\n"
-            playcount = "**▸ Playcount:** " + osu[0]["playcount"] + " (" + osu[0]["total_seconds_played"] + " s)"
-            score = "**▸ Total Score:** " + osu[0]["total_score"] + " (Ranked: " + osu[0]["ranked_score"] + ")\n"
+            playcount = "**▸ Playcount:** " + osu[0]["playcount"] + " (" + osu[0]["total_seconds_played"] + " s)" + "\n"
+            score = "**▸ Total Score:** " + osu[0]["total_score"] + " (Ranked: " + osu[0]["ranked_score"] + ")"
 
             desc = f"{joined}{rank}{level}{pp}{acc}{ranks}{playcount}{score}"
 
