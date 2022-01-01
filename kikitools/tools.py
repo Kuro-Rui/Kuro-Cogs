63c93f871f11a282e89283930a698d98f3a65771
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-01 20:43:02 +0700

    [KikiTools] Fixed Prefix.

--- kikitools/tools.py
+++ kikitools/tools.py
@@ -342,7 +342,7 @@ class KikiTools(commands.Cog):
             inline=False
         )
         e.add_field(name=f"{kiki} Hosting", value=f"This instance is maintained by {owner}.", inline=False)
-        e.add_field(name=f"{cog} Cogs & Their Creators", value=f"*Use `%findcog <command>` to find out who is author of certain command.*\n\n{c1}", inline=False)
+        e.add_field(name=f"{cog} Cogs & Their Creators", value=f"*Use `k?findcog <command>` to find out who is author of certain command.*\n\n{c1}", inline=False)
         e.add_field(name="‎", value=f"{c2}", inline=False)
         e.add_field(name="‎", value=f"{c3}\n‎", inline=False)
         e.set_thumbnail(url=i)
