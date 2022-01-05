8ac29444dcf4b4084921b77f1b5c33fe5e578f99
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-06 01:08:02 +0700

    [Osu] I forgor to change author text

--- osu/osu.py
+++ osu/osu.py
@@ -64,7 +64,7 @@ class Osu(BaseCog):
                 embed.set_author(
                     icon_url="https://icon-library.com/images/osu-icon/osu-icon-16.jpg",
                     url="https://osu.ppy.sh/u/{}".format(osu[0]["user_id"]),
-                    name="osu!Standard Profile for {}".format(osu[0]["username"])
+                    name="osu! Standard Profile for {}".format(osu[0]["username"])
                 )
                 embed.set_footer(text="Powered by osu!", icon_url="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png")
                 embed.set_thumbnail(url="https://a.ppy.sh/{}".format(osu[0]["user_id"]))
@@ -117,7 +117,7 @@ class Osu(BaseCog):
                 embed.set_author(
                     icon_url="https://lemmmy.pw/osusig/img/taiko.png",
                     url="https://osu.ppy.sh/u/{}".format(osu[0]["user_id"]),
-                    name="osu!Standard Profile for {}".format(osu[0]["username"])
+                    name="osu! Taiko Profile for {}".format(osu[0]["username"])
                 )
                 embed.set_footer(text="Powered by osu!", icon_url="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png")
                 embed.set_thumbnail(url="https://a.ppy.sh/{}".format(osu[0]["user_id"]))
@@ -170,7 +170,7 @@ class Osu(BaseCog):
                 embed.set_author(
                     icon_url="https://www.seekpng.com/png/full/194-1941038_osu-ctb-back-white-icon-png.png",
                     url="https://osu.ppy.sh/u/{}".format(osu[0]["user_id"]),
-                    name="osu!Standard Profile for {}".format(osu[0]["username"])
+                    name="osu! Catch The Beat Profile for {}".format(osu[0]["username"])
                 )
                 embed.set_footer(text="Powered by osu!", icon_url="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png")
                 embed.set_thumbnail(url="https://a.ppy.sh/{}".format(osu[0]["user_id"]))
@@ -223,7 +223,7 @@ class Osu(BaseCog):
                 embed.set_author(
                     icon_url="https://icon-library.com/images/osu-icon/osu-icon-15.jpg",
                     url="https://osu.ppy.sh/u/{}".format(osu[0]["user_id"]),
-                    name="osu!Standard Profile for {}".format(osu[0]["username"])
+                    name="osu! Mania Profile for {}".format(osu[0]["username"])
                 )
                 embed.set_footer(text="Powered by osu!", icon_url="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png")
                 embed.set_thumbnail(url="https://a.ppy.sh/{}".format(osu[0]["user_id"]))
