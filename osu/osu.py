b5f6dbad18556d874335bbe4f6f3149e8b660ac9
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-06 00:47:57 +0700

    [Osu] Added something

--- osu/osu.py
+++ osu/osu.py
@@ -32,44 +32,45 @@ class Osu(BaseCog):
         # Queries api to get osu profile
         headers = {"content-type": "application/json", "user-key": apikey}
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(f"https://osu.ppy.sh/api/get_user?k={apikey}&u={username}", headers=headers) as response:
-                osu = await response.json()
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as session:
+                async with session.post(f"https://osu.ppy.sh/api/get_user?k={apikey}&u={username}", headers=headers) as response:
+                    osu = await response.json()
 
-        if osu:
-            SSH = "<:RankSSH:926177230357405736>"
-            SS = "<:RankSS:926177315757645844>"
-            SH = "<:RankSH:926177357834895370>"
-            S = "<:RankS:926177374196875284>"
-            A = "<:RankA:926177386737848321>"
+            if osu:
+                SSH = "<:RankSSH:926177230357405736>"
+                SS = "<:RankSS:926177315757645844>"
+                SH = "<:RankSH:926177357834895370>"
+                S = "<:RankS:926177374196875284>"
+                A = "<:RankA:926177386737848321>"
 
-            # Some format stolen from owo#0498's ">osu" command. (Thanks Stevy 😹)
-            joined = "**▸ Joined at:** {}\n".format(osu[0]["join_date"][:10])
-            rank = "**▸ Rank:** #{}".format(humanize_number(osu[0]["pp_rank"])) + " (:flag_{}: ".format(osu[0]["country"]).lower() + "#{})\n".format(humanize_number(osu[0]["pp_country_rank"]))
-            level = "**▸ Level:** {}\n".format(osu[0]["level"][:5])
-            pp = "**▸ PP:** {}\n".format(osu[0]["pp_raw"])
-            acc = "**▸ Accuracy:** {} %\n".format(osu[0]["accuracy"][:6])
-            playcount = "**▸ Playcount:** {}\n".format(humanize_number(osu[0]["playcount"]))
-            playtime = "**▸ Playtime:** {}\n".format(humanize_timedelta(seconds=osu[0]["total_seconds_played"]))
-            ranks = f"**▸ Ranks:** {SSH}" + "`{}`".format(osu[0]["count_rank_ssh"]) + f"{SS}" + "`{}`".format(osu[0]["count_rank_ss"]) + f"{SH}" + "`{}`".format(osu[0]["count_rank_sh"]) + f"{S}" + "`{}`".format(osu[0]["count_rank_s"]) + f"{A}" + "`{}`\n".format(osu[0]["count_rank_a"])
-            rscore = "**▸ Ranked Score:** {}\n".format(humanize_number(osu[0]["ranked_score"]))
-            tscore = "**▸ Total Score:** {} ".format(humanize_number(osu[0]["total_score"]))
+                # Some format stolen from owo#0498's ">osu" command. (Thanks Stevy 😹)
+                joined = "**▸ Joined at:** {}\n".format(osu[0]["join_date"][:10])
+                rank = "**▸ Rank:** #{}".format(humanize_number(osu[0]["pp_rank"])) + " (:flag_{}: ".format(osu[0]["country"]).lower() + "#{})\n".format(humanize_number(osu[0]["pp_country_rank"]))
+                level = "**▸ Level:** {}\n".format(osu[0]["level"][:5])
+                pp = "**▸ PP:** {}\n".format(osu[0]["pp_raw"])
+                acc = "**▸ Accuracy:** {} %\n".format(osu[0]["accuracy"][:6])
+                playcount = "**▸ Playcount:** {}\n".format(humanize_number(osu[0]["playcount"]))
+                playtime = "**▸ Playtime:** {}\n".format(humanize_timedelta(seconds=osu[0]["total_seconds_played"]))
+                ranks = f"**▸ Ranks:** {SSH}" + "`{}`".format(osu[0]["count_rank_ssh"]) + f"{SS}" + "`{}`".format(osu[0]["count_rank_ss"]) + f"{SH}" + "`{}`".format(osu[0]["count_rank_sh"]) + f"{S}" + "`{}`".format(osu[0]["count_rank_s"]) + f"{A}" + "`{}`\n".format(osu[0]["count_rank_a"])
+                rscore = "**▸ Ranked Score:** {}\n".format(humanize_number(osu[0]["ranked_score"]))
+                tscore = "**▸ Total Score:** {} ".format(humanize_number(osu[0]["total_score"]))
 
-            # Build Embed
-            desc = f"{joined}{rank}{level}{pp}{acc}{playcount}{playtime}{ranks}{rscore}{tscore}"
-            colour = await self.bot.get_embed_colour(await ctx.embed_color())
+                # Build Embed
+                desc = f"{joined}{rank}{level}{pp}{acc}{playcount}{playtime}{ranks}{rscore}{tscore}"
+                colour = await self.bot.get_embed_colour(await ctx.embed_color())
 
-            embed = discord.Embed(description=f"{desc}", colour=colour)
-            embed.set_author(
-                icon_url="https://icon-library.com/images/osu-icon/osu-icon-16.jpg",
-                url="https://osu.ppy.sh/u/{}".format(osu[0]["user_id"]),
-                name="osu!Standard Profile for {}".format(osu[0]["username"])
-            )
-            embed.set_footer(text="Powered by osu!", icon_url="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png")
-            embed.set_thumbnail(url="https://a.ppy.sh/{}".format(osu[0]["user_id"]))
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("No results found for this player.")
+                embed = discord.Embed(description=f"{desc}", colour=colour)
+                embed.set_author(
+                    icon_url="https://icon-library.com/images/osu-icon/osu-icon-16.jpg",
+                    url="https://osu.ppy.sh/u/{}".format(osu[0]["user_id"]),
+                    name="osu!Standard Profile for {}".format(osu[0]["username"])
+                )
+                embed.set_footer(text="Powered by osu!", icon_url="https://upload.wikimedia.org/wikipedia/commons/4/41/Osu_new_logo.png")
+                embed.set_thumbnail(url="https://a.ppy.sh/{}".format(osu[0]["user_id"]))
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("No results found for this player.")
 
     @commands.command(aliases=["osuimg"])
     @commands.bot_has_permissions(embed_links=True)
