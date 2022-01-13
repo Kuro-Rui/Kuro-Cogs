42baeb941b7110c8c688badd7b8eaffcb74a2f03
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-13 23:48:46 +0700

    [CustomImages] Pls work

--- cursedimages/cursedimages.py
+++ cursedimages/cursedimages.py
@@ -25,7 +25,6 @@ class CursedImages(commands.Cog):
                     d = "⚠️**TW⚠️ : CURSED FOOD IMAGES**"
                     e = discord.Embed(title=t, description=d, color=await ctx.embed_color())
                     fn = "cursed_food.png"
-                    f = ctx.message.attachments
-                    f.filename = f"SPOILER_{fn}"
-                    spoiler = await f.to_file()
-                    await ctx.send(embed=e, file=spoiler)
+                    f = discord.File(fp=BytesIO(await resp.read()), filename=f"SPOILER_{fn}")
+                    e.set_image(url=f"attachment://SPOILER_cursed_food.png")
+                    await ctx.send(embed=e, file=f)
