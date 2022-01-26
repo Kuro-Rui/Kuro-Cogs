43f00b579d684cba31ee3fab039c156862db49ed
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-26 19:00:19 +0700

    [Fumo] Added some credits and info.json

--- fumo/fumo.py
+++ fumo/fumo.py
@@ -6,18 +6,24 @@ from redbot.core import commands
 
 
 class Fumo(commands.Cog):
-    """Basically Fumos."""
+    """Basically Fumos.
+    
+    Kuro is ~~not~~ a Fumo Simp.
+    """
 
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command()
+    @commands.cooldown(1, 3, commands.BucketType.user)
     async def fumo(self, ctx):
         """Generates a random Fumo image."""
-        async with aiohttp.ClientSession() as s:
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as s:
                 async with s.get(f"https://fumoapi.herokuapp.com/random") as r:
                     fumo = await r.json()
-
-                    e = discord.Embed(title="Here's a Fumo Image!", color=await ctx.embed_color())
+                    # Embed Thingy :D
+                    e = discord.Embed(title="Here's a Fumo Image! 🎏", color=await ctx.embed_color())
                     e.set_image(url=fumo["URL"])
+                    e.set_footer(text="Powered by fumoapi.herokuapp.com", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
                     await ctx.send(embed=e)
