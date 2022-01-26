5e4e5cde0a8c82079931ea10282f103a6848fcb9
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-26 18:44:27 +0700

    [Fumo] Ig this won't even work anyways

---
+++ fumo/__init__.py
@@ -0,0 +1,6 @@
+from .fumo import Fumo
+
+def setup(bot):
+    bot.add_cog(Fumo(bot))
+
+__red_end_user_data_statement__ = "This cog does not store any end user data."
---
+++ fumo/fumo.py
@@ -0,0 +1,23 @@
+import aiohttp
+
+import discord
+
+from redbot.core import commands
+
+
+class Fumo(commands.Cog):
+    """Basically Fumos."""
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def fumo(self, ctx):
+        """Generates a random Fumo image."""
+        async with aiohttp.ClientSession() as s:
+                async with s.get(f"https://fumoapi.herokuapp.com/random") as r:
+                    fumo = await r.json()
+
+                    e = discord.Embed(title="Here's a Fumo Image!", color=await ctx.embed_color())
+                    e.set_image(url=fumo[0]["URL"])
+                    await ctx.send(embed=e)
