0f951774da2325c716f58f65bf7d0cb1b8ecc611
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-30 02:42:46 +0700

    [Fumo] No need for intense typing

--- fumo/__init__.py
+++ fumo/__init__.py
@@ -26,31 +26,28 @@ class Fumo(commands.Cog):
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def random(self, ctx):
         """Generates a random Fumo image."""
-        async with ctx.typing():
-            e = discord.Embed(title="Here's a Random Fumo Image! 🎏", color=await ctx.embed_color())
-            e.set_image(url=choice(FUMO["list"]))
-            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
-            await ctx.send(embed=e)
+        e = discord.Embed(title="Here's a Random Fumo Image! 🎏", color=await ctx.embed_color())
+        e.set_image(url=choice(FUMO["list"]))
+        e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+        await ctx.send(embed=e)
 
     @fumo.command(aliases=["gifs"])
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def gif(self, ctx):
         """Generates a random Fumo GIF."""
-        async with ctx.typing():
-            e = discord.Embed(title="Here's a Random Fumo GIF! 🎏", color=await ctx.embed_color())
-            e.set_image(url=choice(FUMO["gifs"]))
-            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
-            await ctx.send(embed=e)
+        e = discord.Embed(title="Here's a Random Fumo GIF! 🎏", color=await ctx.embed_color())
+        e.set_image(url=choice(FUMO["gifs"]))
+        e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+        await ctx.send(embed=e)
 
     @fumo.command(aliases=["memes"])
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def meme(self, ctx):
         """Generates a random Fumo meme."""
-        async with ctx.typing():
-            e = discord.Embed(title="Here's a Random Fumo Meme! 🎏", color=await ctx.embed_color())
-            e.set_image(url=choice(FUMO["memes"]))
-            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
-            await ctx.send(embed=e)
+        e = discord.Embed(title="Here's a Random Fumo Meme! 🎏", color=await ctx.embed_color())
+        e.set_image(url=choice(FUMO["memes"]))
+        e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+        await ctx.send(embed=e)
 
     @fumo.command(aliases=["videos"])
     @commands.cooldown(1, 5, commands.BucketType.user)
