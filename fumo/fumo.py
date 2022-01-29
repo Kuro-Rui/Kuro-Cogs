3c1841beb61c86fde13aa9cbc8a9c84d0db01887
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-30 02:26:20 +0700

    [Fumo] Ded API so let's make a new dict

--- fumo/fumo.py
+++ fumo/fumo.py
@@ -1,12 +1,14 @@
 import aiohttp
 
 import discord
-
 from redbot.core import commands
+
+from .fumos import FUMO
 
 
 class Fumo(commands.Cog):
-    """Basically Fumos.
+    """
+    Basically Fumos.
     
     Kuro is ~~not~~ a Fumo Simp.
     """
@@ -14,16 +16,46 @@ class Fumo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.group()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def fumo(self, ctx):
+        """Generates Fumo Image."""
+        pass
+
+    @fumo.commands()
+    async def random(self, ctx):
         """Generates a random Fumo image."""
         async with ctx.typing():
-            async with aiohttp.ClientSession() as s:
-                async with s.get(f"https://fumoapi.herokuapp.com/random") as r:
-                    fumo = await r.json()
-                    # Embed Thingy :D
-                    e = discord.Embed(title="Here's a Fumo Image! 🎏", color=await ctx.embed_color())
-                    e.set_image(url=fumo["URL"])
-                    e.set_footer(text="Powered by fumoapi.herokuapp.com", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
-                    await ctx.send(embed=e)
+            e = discord.Embed(title="Here's a Random Fumo Image! 🎏", color=await ctx.embed_color())
+            e.set_image(url=FUMO["list"])
+            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumos.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+            await ctx.send(embed=e)
+
+    @fumo.commands(aliases=["gifs"])
+    async def gif(self, ctx):
+        """Generates a random Fumo GIF."""
+        async with ctx.typing():
+            e = discord.Embed(title="Here's a Random Fumo GIF! 🎏", color=await ctx.embed_color())
+            e.set_image(url=FUMO["gifs"])
+            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumos.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+            await ctx.send(embed=e)
+
+    @fumo.commands(aliases=["memes"])
+    async def meme(self, ctx):
+        """Generates a random Fumo meme."""
+        async with ctx.typing():
+            e = discord.Embed(title="Here's a Random Fumo Meme! 🎏", color=await ctx.embed_color())
+            e.set_image(url=FUMO["memes"])
+            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumos.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+            await ctx.send(embed=e)
+
+    @fumo.commands(aliases=["videos"])
+    async def video(self, ctx):
+        """Generates a random Fumo video."""
+        async with ctx.typing():
+            e = discord.Embed(title="Here's a Random Fumo Image! 🎏", color=await ctx.embed_color())
+            f = discord.File(FUMO["videos"], filename="fumo.mp4")
+            e.set_image(url="attachment://fumo.mp4")
+            e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/images.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
+            await ctx.send(embed=e, file=f)
+            f.close()
