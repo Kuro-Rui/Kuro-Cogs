eb67f1553ba553df6f13d77913dd321bb77c25bb
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-01-30 02:38:44 +0700

    [Fumo] Import random wen

--- fumo/__init__.py
+++ fumo/__init__.py
@@ -1,4 +1,6 @@
 import aiohttp
+import random
+from random import choice
 
 import discord
 from redbot.core import commands
@@ -26,7 +28,7 @@ class Fumo(commands.Cog):
         """Generates a random Fumo image."""
         async with ctx.typing():
             e = discord.Embed(title="Here's a Random Fumo Image! 🎏", color=await ctx.embed_color())
-            e.set_image(url=FUMO["list"])
+            e.set_image(url=choice(FUMO["list"]))
             e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
             await ctx.send(embed=e)
 
@@ -36,7 +38,7 @@ class Fumo(commands.Cog):
         """Generates a random Fumo GIF."""
         async with ctx.typing():
             e = discord.Embed(title="Here's a Random Fumo GIF! 🎏", color=await ctx.embed_color())
-            e.set_image(url=FUMO["gifs"])
+            e.set_image(url=choice(FUMO["gifs"]))
             e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
             await ctx.send(embed=e)
 
@@ -46,7 +48,7 @@ class Fumo(commands.Cog):
         """Generates a random Fumo meme."""
         async with ctx.typing():
             e = discord.Embed(title="Here's a Random Fumo Meme! 🎏", color=await ctx.embed_color())
-            e.set_image(url=FUMO["memes"])
+            e.set_image(url=choice(FUMO["memes"]))
             e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
             await ctx.send(embed=e)
 
@@ -56,7 +58,7 @@ class Fumo(commands.Cog):
         """Generates a random Fumo video."""
         async with ctx.typing():
             e = discord.Embed(title="Here's a Random Fumo Video! 🎏", color=await ctx.embed_color())
-            f = discord.File(FUMO["videos"], filename="fumo.mp4")
+            f = discord.File(choice(FUMO["videos"]), filename="fumo.mp4")
             e.set_image(url="attachment://fumo.mp4")
             e.set_footer(text="Source: github.com/Kuro-Rui/Kuro-Cogs/blob/main/fumo/fumo.py", icon_url="https://cdn.discordapp.com/emojis/935839733173612594.gif?size=128&quality=lossless")
             await ctx.send(embed=e, file=f)
