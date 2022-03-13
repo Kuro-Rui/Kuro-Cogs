e9289b65a772fb3936e64bddbb9fc94c7bda1555
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-03-13 17:11:05 +0700

    [CounterCog] Add this

--- countercog/counter.py
+++ countercog/counter.py
@@ -7,13 +7,13 @@ class CounterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    __author__ = ["Kuro"]
+    __author__ = "Kuro"
     __version__ = "1.0.0"
 
     def format_help_for_context(self, ctx: commands.Context):
         """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\n\nCog Version: {self.__version__}"
+        return f"{pre_processed}\n\n`Cog Author  :` {self.__author__}\n`Cog Version :` {self.__version__}"
 
     @commands.is_owner()
     @commands.group()
