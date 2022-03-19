3d8659f033f3eeec188201847f3c0bd7e85c25ce
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-03-20 00:43:47 +0700

    [BotPing] Remove speedtest-cli, add config

--- botping/ping.py
+++ botping/ping.py
@@ -23,22 +23,27 @@ SOFTWARE.
 
 import random
 import asyncio
-import concurrent
 import datetime
 import logging
 import time
 
 import discord
-import speedtest
 from redbot.core import Config, commands
-from redbot.core.utils import chat_formatting as chat
-from redbot.core.utils.chat_formatting import humanize_list
+from redbot.core.utils import AsyncIter, chat_formatting as chat
 
 old_ping = None
-log = logging.getLogger("red.kuro.customping")
+log = logging.getLogger("red.kuro.botping")
 
 class BotPing(commands.Cog):
     """A more information rich ping message."""
+
+    __author__ = ["PhenoM4n4n", "Kuro"]
+    __version__ = "1.1.0"
+
+    def format_help_for_context(self, ctx: commands.Context):
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\n`Cog Authors :` {self.__author__}\n`Cog Version :` {self.__version__}"
 
     def __init__(self, bot):
         self.bot = bot
@@ -47,21 +52,9 @@ class BotPing(commands.Cog):
             identifier=325236743863625234572,
             force_registration=True,
         )
-        default_global = {"host_latency": True}
+        default_global = {"use_gifs": True}
         self.config.register_global(**default_global)
         self.settings = {}
-
-    __author__ = humanize_list(["PhenoM4n4n", "Kuro"])
-    __version__ = "1.0.1"
-
-    def format_help_for_context(self, ctx: commands.Context):
-        """Thanks Sinbad!"""
-        pre_processed = super().format_help_for_context(ctx)
-        return (
-            f"{pre_processed}\n\n"
-            f"`Cog Authors :` {self.__author__}\n"
-            f"`Cog Version :` {self.__version__}"
-        )
 
     async def initialize(self):
         self.settings = await self.config.all()
@@ -89,20 +82,21 @@ class BotPing(commands.Cog):
         e = discord.Embed(title="Pinging...")
         totalPing = round((end - start) * 1000, 2)
         e.add_field(name="Overall:", value=chat.box(f"{totalPing}" + " ms", "py"))
-        ping_gifs = (
-            "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
-            "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
-            "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
-            "https://i.imgur.com/LkdjWE6.gif",
-            "https://i.gifer.com/6TaL.gif",
-            "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
-            "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
-            "https://imgur.com/1cnscjV.gif",
-            "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
-            "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
-        )
-        ping_gifs_picker = random.choice(ping_gifs)
-        e.set_image(url=ping_gifs_picker)
+        if await self.config.use_gifs():
+            ping_gifs = (
+                "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
+                "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
+                "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
+                "https://i.imgur.com/LkdjWE6.gif",
+                "https://i.gifer.com/6TaL.gif",
+                "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
+                "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
+                "https://imgur.com/1cnscjV.gif",
+                "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
+                "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
+            )
+            ping_gifs_picker = random.choice(ping_gifs)
+            e.set_image(url=ping_gifs_picker)
         await asyncio.sleep(0.25)
         try:
             await message.edit(content=None, embed=e)
@@ -110,7 +104,7 @@ class BotPing(commands.Cog):
             return
 
         botPing = round(self.bot.latency * 1000, 2)
-        e.add_field(name="Discord WS:", value=chat.box(f"{botPing}" + " ms", "py"))
+        e.add_field(name="Discord WebSocket:", value=chat.box(f"{botPing}" + " ms", "py"))
         await asyncio.sleep(0.25)
 
         averagePing = (botPing + totalPing) / 2
@@ -121,33 +115,9 @@ class BotPing(commands.Cog):
         else:
             color = discord.Colour.green()
 
-        if not self.settings["host_latency"]:
-            e.title = "Pong! 🏓"
-
-        e.color = color
-        try:
-            await message.edit(embed=e)
-        except discord.NotFound:
-            return
-        if not self.settings["host_latency"]:
-            return
-
-        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-        loop = asyncio.get_event_loop()
-        try:
-            s = speedtest.Speedtest(secure=True)
-            await loop.run_in_executor(executor, s.get_servers)
-            await loop.run_in_executor(executor, s.get_best_server)
-        except Exception as exc:
-            log.exception("An exception occured while fetching host latency.", exc_info=exc)
-            host_latency = "Failed"
-        else:
-            result = s.results.dict()
-            host_latency = round(result["ping"], 2)
-
         e.title = "Pong! 🏓"
-        e.add_field(name="Host:", value=chat.box(f"{host_latency}" + " ms", "py"))
-        await asyncio.sleep(0.25)
+        e.color = color
+
         try:
             await message.edit(embed=e)
         except discord.NotFound:
@@ -167,20 +137,21 @@ class BotPing(commands.Cog):
         send_end = time.monotonic()
         send_ping = round((send_end - send_start) * 1000, 2)
         e.add_field(name="Send:", value=chat.box(f"{send_ping}" + " ms", "py"))
-        ping_gifs = (
-            "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
-            "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
-            "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
-            "https://i.imgur.com/LkdjWE6.gif",
-            "https://i.gifer.com/6TaL.gif",
-            "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
-            "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
-            "https://imgur.com/1cnscjV.gif",
-            "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
-            "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
-        )
-        ping_gifs_picker = random.choice(ping_gifs)
-        e.set_image(url=ping_gifs_picker)
+        if await self.config.use_gifs():
+            ping_gifs = (
+                "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
+                "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
+                "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
+                "https://i.imgur.com/LkdjWE6.gif",
+                "https://i.gifer.com/6TaL.gif",
+                "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
+                "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
+                "https://imgur.com/1cnscjV.gif",
+                "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
+                "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
+            )
+            ping_gifs_picker = random.choice(ping_gifs)
+            e.set_image(url=ping_gifs_picker)
         await asyncio.sleep(0.25)
 
         edit_start = time.monotonic()
@@ -227,39 +198,40 @@ class BotPing(commands.Cog):
             color = discord.Colour.green()
 
         e = discord.Embed(color=color, title="Shard Pings", description="\n".join(description))
-        ping_gifs = (
-            "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
-            "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
-            "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
-            "https://i.imgur.com/LkdjWE6.gif",
-            "https://i.gifer.com/6TaL.gif",
-            "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
-            "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
-            "https://imgur.com/1cnscjV.gif",
-            "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
-            "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
-        )
-        ping_gifs_picker = random.choice(ping_gifs)
-        e.set_image(url=ping_gifs_picker)
+        if await self.config.use_gifs():
+            ping_gifs = (
+                "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
+                "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
+                "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
+                "https://i.imgur.com/LkdjWE6.gif",
+                "https://i.gifer.com/6TaL.gif",
+                "https://i.kym-cdn.com/photos/images/original/000/753/601/bc8.gif",
+                "https://c.tenor.com/On7v3wlDxNUAAAAd/ping-pong-anime.gif",
+                "https://imgur.com/1cnscjV.gif",
+                "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
+                "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
+            )
+            ping_gifs_picker = random.choice(ping_gifs)
+            e.set_image(url=ping_gifs_picker)
         e.set_footer(text=f"Average: {round(average_ping, 2)} ms")
         await ctx.send(embed=e)
 
     @commands.is_owner()
     @commands.group()
     async def pingset(self, ctx: commands.Context):
-        """Manage CustomPing settings."""
+        """Manage BotPing settings."""
 
-    @pingset.command(name="hostlatency")
-    async def pingset_hostlatency(self, ctx: commands.Context, true_or_false: bool = None):
-        """Toggle displaying host latency on the ping command."""
+    @pingset.command(name="usegifs", aliases=["usegif", "gif"])
+    async def pingset_usegifs(self, ctx: commands.Context, true_or_false: bool = None):
+        """Toggle displaying GIFs on the ping embed."""
         target_state = (
-            true_or_false if true_or_false is not None else not (await self.config.host_latency())
+            true_or_false if true_or_false is not None else not (await self.config.use_gifs())
         )
-        await self.config.host_latency.set(target_state)
-        self.settings["host_latency"] = target_state
+        await self.config.use_gifs.set(target_state)
+        self.settings["use_gifs"] = target_state
         word = " " if target_state else " not "
         await ctx.send(
-            f"Host latency will{word}be displayed on the `{ctx.clean_prefix}ping` command."
+            f"Ping Pong GIFs will{word}be displayed on the `{ctx.clean_prefix}ping` embed."
         )
 
 
