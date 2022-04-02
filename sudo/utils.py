197cb0c2758cbc92df6ef952394dd9fc32622478
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-04-03 00:41:51 +0700

    [Sudo] Am I this dumb? This is a fatal error

--- sudo/utils.py
+++ sudo/utils.py
@@ -5,13 +5,13 @@ from redbot.core import commands
 
 
 def is_owner(real: Optional[bool], copied: Optional[bool]):
-    async def predicate(self, ctx):
+    async def predicate(ctx):
         if real:
             if ctx.author.id in ctx.bot.owner_ids:
                 return True
         if copied:
-            # self_all_owner_ids = ctx.bot.get_cog("Sudo").all_owner_ids
-            if ctx.author.id in self.all_owner_ids and ctx.author.id not in ctx.bot.owner_ids:
+            all_owner_ids = ctx.bot.get_cog("Sudo").all_owner_ids
+            if ctx.author.id in all_owner_ids and ctx.author.id not in ctx.bot.owner_ids:
                 return True
         return False
     return commands.check(predicate)
