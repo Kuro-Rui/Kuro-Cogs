428d2f672ba0fee0458b86454b566ae06679070c
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-04-04 20:57:30 +0700

    [Osu] This would work + more efficient tho

--- osu/utils.py
+++ osu/utils.py
@@ -94,121 +94,115 @@ async def get_osu_avatar(self, ctx, username: str = None):
     """Get an osu! Avatar"""
 
     osu = await osu_api_call(self, ctx, username=username)
-    if not osu:
-        return osu  # Return error embed, to prevent subscription error.
-
-    avatar_url = "https://a.ppy.sh/{}".format(osu[0]["user_id"])
-    filename = "{}_osu-avatar.png".format(osu[0]["username"])
-    async with self.session.get(avatar_url) as image:
-        avatar = discord.File(fp=BytesIO(await image.read()), filename=filename)
-    return avatar, avatar_url, filename
+    if osu:
+        avatar_url = "https://a.ppy.sh/{}".format(osu[0]["user_id"])
+        filename = "{}_osu-avatar.png".format(osu[0]["username"])
+        async with self.session.get(avatar_url) as image:
+            avatar = discord.File(fp=BytesIO(await image.read()), filename=filename)
+        return avatar, avatar_url, filename
 
 
 async def send_osu_user_info(self, ctx, m: int = 0, username: str = None):
     """osu! User Info Embed"""
 
     osu = await osu_api_call(self, ctx, m, username)
-    if not osu:
-        return osu  # Return error embed, to prevent subscription error.
+    if osu:
+        # avatar_url isn't actually used, just to prevent "too many values to unpack"
+        avatar, avatar_url, filename = await get_osu_avatar(self, ctx, username)
+        ssh, ss, sh, s, a = await rank_emojis(self)
 
-    # avatar_url isn't actually used, just to prevent "too many values to unpack"
-    avatar, avatar_url, filename = await get_osu_avatar(self, ctx, username)
-    ssh, ss, sh, s, a = await rank_emojis(self)
-
-    # Inspired by owo#0498 (Thanks Stevy 😹)
-    description = (
-        (
-            "**▸ Joined at:** {}\n"
-            "**▸ Rank:** #{} (:flag_{}: #{})\n"
-            "**▸ Level:** {}\n"
-            "**▸ PP:** {}\n"
-            "**▸ Accuracy:** {}%\n"
-            "**▸ Playcount:** {}\n"
-            "**▸ Playtime:** {}\n"
-            "**▸ Ranks:** {}`{}` {}`{}` {}`{}` {}`{}` {}`{}`\n"
-            "**▸ Ranked Score:** {}\n"
-            "**▸ Total Score:** {}"
+        # Inspired by owo#0498 (Thanks Stevy 😹)
+        description = (
+            (
+                "**▸ Joined at:** {}\n"
+                "**▸ Rank:** #{} (:flag_{}: #{})\n"
+                "**▸ Level:** {}\n"
+                "**▸ PP:** {}\n"
+                "**▸ Accuracy:** {}%\n"
+                "**▸ Playcount:** {}\n"
+                "**▸ Playtime:** {}\n"
+                "**▸ Ranks:** {}`{}` {}`{}` {}`{}` {}`{}` {}`{}`\n"
+                "**▸ Ranked Score:** {}\n"
+                "**▸ Total Score:** {}"
+            )
+            .format(
+                osu[0]["join_date"][:10],
+                humanize_number(osu[0]["pp_rank"]),
+                osu[0]["country"].lower(),
+                humanize_number(osu[0]["pp_country_rank"]),
+                osu[0]["level"][:2],
+                osu[0]["pp_raw"],
+                osu[0]["accuracy"][:5],
+                humanize_number(osu[0]["playcount"]),
+                humanize_timedelta(seconds=osu[0]["total_seconds_played"]),
+                ssh,
+                osu[0]["count_rank_ssh"],
+                ss,
+                osu[0]["count_rank_ss"],
+                sh,
+                osu[0]["count_rank_sh"],
+                s,
+                osu[0]["count_rank_s"],
+                a,
+                osu[0]["count_rank_a"],
+                humanize_number(osu[0]["ranked_score"]),
+                humanize_number(osu[0]["total_score"]),
+            )
+            .replace(",", ".")
         )
-        .format(
-            osu[0]["join_date"][:10],
-            humanize_number(osu[0]["pp_rank"]),
-            osu[0]["country"].lower(),
-            humanize_number(osu[0]["pp_country_rank"]),
-            osu[0]["level"][:2],
-            osu[0]["pp_raw"],
-            osu[0]["accuracy"][:5],
-            humanize_number(osu[0]["playcount"]),
-            humanize_timedelta(seconds=osu[0]["total_seconds_played"]),
-            ssh,
-            osu[0]["count_rank_ssh"],
-            ss,
-            osu[0]["count_rank_ss"],
-            sh,
-            osu[0]["count_rank_sh"],
-            s,
-            osu[0]["count_rank_s"],
-            a,
-            osu[0]["count_rank_a"],
-            humanize_number(osu[0]["ranked_score"]),
-            humanize_number(osu[0]["total_score"]),
+
+        if m == 0:
+            type = icon = "osu"
+            mode = "Standard"
+        elif m == 1:
+            type = icon = "taiko"
+            mode = "Taiko"
+        elif m == 2:
+            type = "fruits"
+            icon = "ctb"
+            mode = "Catch"
+        elif m == 3:
+            type = icon = "mania"
+            mode = "Mania"
+
+        embed = discord.Embed(description=description, color=await ctx.embed_color())
+        embed.set_author(
+            icon_url="https://lemmmy.pw/osusig/img/{}.png".format(icon),
+            url="https://osu.ppy.sh/users/{}/{}".format(osu[0]["user_id"], type),
+            name="osu! {} Profile for {}".format(mode, osu[0]["username"]),
         )
-        .replace(",", ".")
-    )
-
-    if m == 0:
-        type = icon = "osu"
-        mode = "Standard"
-    elif m == 1:
-        type = icon = "taiko"
-        mode = "Taiko"
-    elif m == 2:
-        type = "fruits"
-        icon = "ctb"
-        mode = "Catch"
-    elif m == 3:
-        type = icon = "mania"
-        mode = "Mania"
-
-    embed = discord.Embed(description=description, color=await ctx.embed_color())
-    embed.set_author(
-        icon_url="https://lemmmy.pw/osusig/img/{}.png".format(icon),
-        url="https://osu.ppy.sh/users/{}/{}".format(osu[0]["user_id"], type),
-        name="osu! {} Profile for {}".format(mode, osu[0]["username"]),
-    )
-    embed.set_footer(
-        text="Powered by osu!", icon_url="https://img.icons8.com/color/48/000000/osu.png"
-    )
-    embed.set_thumbnail(url=f"attachment://{filename}")
-    await ctx.send(embed=embed, file=avatar)
+        embed.set_footer(
+            text="Powered by osu!", icon_url="https://img.icons8.com/color/48/000000/osu.png"
+        )
+        embed.set_thumbnail(url=f"attachment://{filename}")
+        await ctx.send(embed=embed, file=avatar)
 
 
 async def send_osu_user_card(self, ctx, username: str = None):
     """Sends an osu! Profile Card from Martine API"""
 
     osu = await osu_api_call(self, ctx, username=username)
-    if not osu:
-        return osu  # Return error embed, to prevent subscription error.
-
-    async with self.session.get(
-        "https://api.martinebot.com/v1/imagesgen/osuprofile?player_username={}".format(
-            osu[0]["username"]
-        )
-    ) as response:
-        if response.status in [200, 201]:
-            embed = discord.Embed(color=await ctx.embed_color())
-            embed.set_author(
-                name="{}'s osu! Standard Card:".format(osu[0]["username"]),
-                url="https://osu.ppy.sh/users/{}".format(osu[0]["user_id"]),
+    if osu:
+        async with self.session.get(
+            "https://api.martinebot.com/v1/imagesgen/osuprofile?player_username={}".format(
+                osu[0]["username"]
             )
-            filename = "{}_osu-card.png".format(osu[0]["username"])
-            card = discord.File(fp=BytesIO(await response.read()), filename=filename)
-            embed.set_image(url=f"attachment://{filename}")
-            embed.set_footer(
-                text="Powered by api.martinebot.com",
-                icon_url="https://img.icons8.com/color/48/000000/osu.png",
-            )
-            await ctx.send(embed=embed, file=card)
-        elif response.status in [404, 410, 422]:
-            await ctx.send((await response.json())["message"])
-        else:
-            await ctx.send("API is currently down, please try again later.")
+        ) as response:
+            if response.status in [200, 201]:
+                embed = discord.Embed(color=await ctx.embed_color())
+                embed.set_author(
+                    name="{}'s osu! Standard Card:".format(osu[0]["username"]),
+                    url="https://osu.ppy.sh/users/{}".format(osu[0]["user_id"]),
+                )
+                filename = "{}_osu-card.png".format(osu[0]["username"])
+                card = discord.File(fp=BytesIO(await response.read()), filename=filename)
+                embed.set_image(url=f"attachment://{filename}")
+                embed.set_footer(
+                    text="Powered by api.martinebot.com",
+                    icon_url="https://img.icons8.com/color/48/000000/osu.png",
+                )
+                await ctx.send(embed=embed, file=card)
+            elif response.status in [404, 410, 422]:
+                await ctx.send((await response.json())["message"])
+            else:
+                await ctx.send("API is currently down, please try again later.")
