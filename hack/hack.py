b15a4824857ae0e414bd42563467397593107a63
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-04-14 01:41:11 +0700

    [Hack] Lower duration ig, boring...

--- hack/hack.py
+++ hack/hack.py
@@ -32,24 +32,24 @@ class Hack(commands.Cog):
         )
 
     @commands.guild_only()
-    @commands.cooldown(1, 35, commands.BucketType.channel)
+    @commands.cooldown(1, 25, commands.BucketType.channel)
     @commands.command(aliases=["heck"])
     async def hack(self, ctx, member: discord.Member):
         """Hack someone!"""
 
         # Mass editing lol, must add handler on each :D
         message = await ctx.send(f"{loading(0)} Hacking {member.nick} now...")
-        await sleep(3)
+        await sleep(2)
         try:
             await message.edit(content=f"{loading(1)} Finding Discord Login...")
         except discord.NotFound:
             return
-        await sleep(3)
+        await sleep(2)
         try:
             await message.edit(content=f"{loading(2)} Bypassing 2FA...")
         except discord.NotFound:
             return
-        await sleep(4)
+        await sleep(3)
         email = member.name.replace(" ", "_").replace("'", "") + "@gmail.com"
         password = "".join(choice(ascii_letters) for letters in range(10))
         try:
@@ -62,12 +62,12 @@ class Hack(commands.Cog):
             )
         except discord.NotFound:
             return
-        await sleep(5)
+        await sleep(4)
         try:
             await message.edit(content=f"{loading(0)} Fetching user DMs...")
         except discord.NotFound:
             return
-        await sleep(2)
+        await sleep(1)
         last_dm = choice(
             [
                 "man I love my mommy.",
@@ -93,28 +93,28 @@ class Hack(commands.Cog):
             await message.edit(content=f"{loading(2)} Injecting trojan virus into {member}...")
         except discord.NotFound:
             return
-        await sleep(3)
+        await sleep(2)
         try:
             await message.edit(content=f"{loading(3)} Virus injected. Finding IP Address...")
         except discord.NotFound:
             return
-        await sleep(4)
+        await sleep(3)
         ip_address = f"{randint(50, 255)}.{randint(50, 255)}.{randint(50, 255)}.{randint(50, 255)}"
         try:
             await message.edit(content=f"{loading(0)} **IP Address**: `{ip_address}`")
         except discord.NotFound:
             return
-        await sleep(3)
+        await sleep(2)
         try:
             await message.edit(content=f"{loading(1)} Selling user data to the government...")
         except discord.NotFound:
             return
-        await sleep(3)
+        await sleep(2)
         try:
             await message.edit(content=f"{loading(2)} Reporting account to Discord for breaking ToS...")
         except discord.NotFound:
             return
-        await sleep(2)
+        await sleep(1)
         try:
             await message.edit(content=f"{commands.context.TICK} Finished hacking {member.nick}.")
         except discord.NotFound:
