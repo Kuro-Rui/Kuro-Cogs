ec3864e493dfdcfdf480201ceed3cc1f929c6a29
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-04-13 23:53:16 +0700

    [BotPing] Why the chosen gifs stays the same?

--- botping/gifs.py
+++ botping/gifs.py
@@ -1,4 +1,4 @@
-ping_pong_gifs = [
+ping_pong_gifs = {
     "https://i.pinimg.com/originals/ac/b8/8f/acb88f71e5ed54072a24f647e28a9c3f.gif",
     "https://4.bp.blogspot.com/-8XanbCQDxfg/WnJTaUeifYI/AAAAAAABEUo/5yv_KUlLV9cmJsuI8jeFRrGSXbtQMclngCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BShokugeki%2Bno%2BSoma%2BS2%2B-%2BOAD%2B1%2B%255BDVD%255D%2B-%2BMegumi%2Bvs%2BIsshiki.gif",
     "https://remyfool.files.wordpress.com/2016/11/agari-rally.gif?w=924",
@@ -9,4 +9,4 @@ ping_pong_gifs = [
     "https://imgur.com/1cnscjV.gif",
     "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1589129513917-X6QBWRXBHLCSFXT9INR2/b17c1b31e185d12aeca55b576c1ecaef.gif",
     "https://i1.wp.com/drunkenanimeblog.com/wp-content/uploads/2017/11/shakunetsu-no-takkyuu-musume-scorching-ping-pong-girls.gif?fit=540%2C303&ssl=1&resize=350%2C200https://media1.tenor.com/images/2b27c6e7747d319f76fd98d2a226ab33/tenor.gif?itemid=15479836",
-]
+}
