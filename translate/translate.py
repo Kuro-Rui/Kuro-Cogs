0b81c685e8d5c9cb62ebd15d8a107a2700029768
Kuro <83048873+Kuro-Rui@users.noreply.github.com>
2022-04-15 03:48:07 +0700

    [Translate] "probably switching these args is not a bad idea"

--- translate/translate.py
+++ translate/translate.py
@@ -41,22 +41,24 @@ class Translate(commands.Cog):
 
     @commands.command()
     async def translate(
-            self,
-            ctx,
-            to_language: LangConverter,
-            from_language: Optional[LangConverter] = "Auto",
-            *,
-            text: str
+        self,
+        ctx,
+        from_language: Optional[LangConverter],
+        to_language: LangConverter,
+        *,
+        text: str
     ):
         """
         Translates the given text!
 
         You can also provide a language to translate from (`from_language`).
         **Examples**:
-            - `[p]translate es en Example Text` (Translates "Example Text" to Español)
+            - `[p]translate en es Example Text` (Translates "Example Text" to Español)
             - `[p]translate en Ejemplo de texto` (Translates "Ejemplo de texto" from Español to English)
         """
 
+        if not from_language:
+            from_language = "Auto"
         try:
             result = await self.bot.loop.run_in_executor(
                 None,
