dd75ee4fd17080bd7b9e7fe360203b3721ce5636
Kuro-Rui <louisdominic80@gmail.com>
2022-05-10 18:11:54 +0700

    [Hack] Make code looks less cursed

--- hack/hack.py
+++ hack/hack.py
@@ -65,22 +65,16 @@ class Hack(commands.Cog):
             await ctx.send("Umm, please don't DOXX yourself \N{SKULL}")
             return
 
-        # Mass editing lol, must add handler on each :D
+        # Mass editing lol
         message = await ctx.send(f"{loading(0)} Hacking {member.name} now...")
         await sleep(2)
         try:
             await message.edit(content=f"{loading(1)} Finding Discord Login...")
-        except discord.NotFound:
-            return
-        await sleep(2)
-        try:
+            await sleep(2)
             await message.edit(content=f"{loading(2)} Bypassing 2FA...")
-        except discord.NotFound:
-            return
-        await sleep(3)
-        email = member.name.replace(" ", "_").replace("'", "") + "@gmail.com"
-        password = "".join(choice(ascii_letters) for letters in range(10))
-        try:
+            await sleep(3)
+            email = member.name.replace(" ", "_").replace("'", "") + "@gmail.com"
+            password = "".join(choice(ascii_letters) for letters in range(10))
             await message.edit(
                 content=(
                     f"{loading(3)} Found login information:\n"
@@ -88,66 +82,43 @@ class Hack(commands.Cog):
                     f"**Password**: `{password}`"
                 )
             )
-        except discord.NotFound:
-            return
-        await sleep(4)
-        try:
+            await sleep(4)
             await message.edit(content=f"{loading(0)} Fetching user DMs...")
-        except discord.NotFound:
-            return
-        await sleep(1)
-        last_dm = choice(
-            [
-                "man I love my mommy.",
-                "can I see your feet pics?",
-                "yeah I'm just built different.",
-                "UwU",
-                "pwetty pwease?",
-                "dont frgt to like and subscrube!!",
-                "I think it's smaller than most.",
-                "I hope blueballs aren't real.",
-                "yeah she goes to another school.",
-                "imagine having a peen as small as mine in 2022",
-                "I hope noone sees my nudes folder.",
-                "honestly I'm pretty sure blue waffle is real and I have it.",
-            ]
-        )
-        try:
+            await sleep(1)
+            last_dm = choice(
+                [
+                    "man I love my mommy.",
+                    "can I see your feet pics?",
+                    "yeah I'm just built different.",
+                    "UwU",
+                    "pwetty pwease?",
+                    "dont frgt to like and subscrube!!",
+                    "I think it's smaller than most.",
+                    "I hope blueballs aren't real.",
+                    "yeah she goes to another school.",
+                    "imagine having a peen as small as mine in 2022",
+                    "I hope noone sees my nudes folder.",
+                    "honestly I'm pretty sure blue waffle is real and I have it.",
+                ]
+            )
             await message.edit(content=f'{loading(1)} **Last DM**: "{last_dm}"')
-        except discord.NotFound:
-            return
-        await sleep(3)
-        try:
+            await sleep(3)
             await message.edit(content=f"{loading(2)} Injecting trojan virus into {member}...")
-        except discord.NotFound:
-            return
-        await sleep(2)
-        try:
+            await sleep(2)
             await message.edit(content=f"{loading(3)} Virus injected. Finding IP Address...")
-        except discord.NotFound:
-            return
-        await sleep(3)
-        # A valid IP address must be in the form of x.x.x.x, where x is a number from 0-255.
-        ip_address = f"{randint(0, 255)}.{randint(0, 255)}.{randint(0, 255)}.{randint(0, 255)}"
-        try:
+            await sleep(3)
+            # A valid IP address must be in the form of x.x.x.x, where x is a number from 0-255.
+            ip_address = f"{randint(0, 255)}.{randint(0, 255)}.{randint(0, 255)}.{randint(0, 255)}"
             await message.edit(content=f"{loading(0)} **IP Address**: `{ip_address}`")
-        except discord.NotFound:
-            return
-        await sleep(2)
-        try:
+            await sleep(2)
             await message.edit(content=f"{loading(1)} Selling user data to the government...")
-        except discord.NotFound:
-            return
-        await sleep(2)
-        try:
+            await sleep(2)
             await message.edit(
                 content=f"{loading(2)} Reporting account to Discord for breaking ToS..."
             )
-        except discord.NotFound:
-            return
-        await sleep(1)
-        try:
+            await sleep(1)
             await message.edit(content=f"{commands.context.TICK} Finished hacking {member.name}.")
+            await ctx.send("The *totally* real and dangerous hack is complete.")
         except discord.NotFound:
+            await ctx.send("Process terminated. The hack failed.")
             return
-        await ctx.send("The *totally* real and dangerous hack is complete.")
