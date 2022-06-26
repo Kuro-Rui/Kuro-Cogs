48c9f20195c35a9f287c493a3b08c7589ef37188
Kuro-Rui <louisdominic80@gmail.com>
2022-06-26 21:59:58 +0700

    [AvatarImgen] Just prettifying <3

--- avatarimgen/avatar_imgen.py
+++ avatarimgen/avatar_imgen.py
@@ -23,6 +23,7 @@ SOFTWARE.
 """
 
 import asyncio
+import random
 from io import BytesIO
 
 import aiohttp
@@ -39,7 +40,7 @@ class AvatarImgen(commands.Cog):
         self.session = aiohttp.ClientSession()
 
     __author__ = humanize_list(["Kuro"])
-    __version__ = "0.0.3"
+    __version__ = "0.0.4"
 
     def format_help_for_context(self, ctx: commands.Context):
         """Thanks Sinbad!"""
@@ -62,19 +63,9 @@ class AvatarImgen(commands.Cog):
     async def ad(self, ctx, user: discord.User = None):
         """Make an advertisement!"""
 
-        if not user:
-            user = ctx.author
-
+        user = user or ctx.author
         avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/ad?image={avatar}") as r:
-                embed = discord.Embed(title="Advertisement", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"ads.png")
-                embed.set_image(url="attachment://ads.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "ad", avatar, title="Advertisement")
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -82,53 +73,25 @@ class AvatarImgen(commands.Cog):
         """This person is a clown, Star."""
 
         avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/clown?image={avatar}") as r:
-                embed = discord.Embed(title="Clown", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"clown.png")
-                embed.set_image(url="attachment://clown.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "clown", avatar, title="Clown", color=user.color)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def drip(self, ctx, user: discord.User = None):
         """Pretend to wear a rich jacket!"""
 
-        if not user:
-            user = ctx.author
-
+        user = user or ctx.author
         avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/drip?image={avatar}") as r:
-                embed = discord.Embed(title="Drip", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"drip.png")
-                embed.set_image(url="attachment://drip.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "drip", avatar, title="Drip", color=user.color)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def gun(self, ctx, user: discord.User = None):
         """Add a gun overlay to your avatar!"""
 
-        if not user:
-            user = ctx.author
-
+        user = user or ctx.author
         avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/gun?image={avatar}") as r:
-                embed = discord.Embed(title="Gun", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"gun.png")
-                embed.set_image(url="attachment://gun.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "gun", avatar, title="Gun", color=user.color)
 
     @commands.command(aliases=["jokesoverhead"])
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -137,96 +100,63 @@ class AvatarImgen(commands.Cog):
 
         # The API isn't accepting "?size=1024" part that's attached to avatar URLs
         avatar = f"https://cdn.discordapp.com/avatars/{user.id}/{user.avatar}.png"
-
-        async with ctx.typing():
-            async with self.session.get(
-                f"https://api.popcat.xyz/jokeoverhead?image={avatar}"
-            ) as r:
-                embed = discord.Embed(title="Jokes Overhead", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"jokeoverhead.png")
-                embed.set_image(url="attachment://jokeoverhead.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
-
-    @commands.command()
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def mnm(self, ctx, user: discord.User = None):
-        """Make anyone turns into a shape of M&M!"""
-
-        if not user:
-            user = ctx.author
-
-        avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/mnm?image={avatar}") as r:
-                embed = discord.Embed(title="M&M", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"mnm.png")
-                embed.set_image(url="attachment://mnm.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "jokeoverhead", avatar, title="Joke Overhead", color=user.color)
 
     @commands.command(aliases=["wall"])
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def uncover(self, ctx, user: discord.User = None):
         """So this person was hiding behind the wall all the time?"""
 
-        if not user:
-            user = ctx.author
-
+        user = user or ctx.author
         avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/uncover?image={avatar}") as r:
-                embed = discord.Embed(title="Behind The Wall", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"wall.png")
-                embed.set_image(url="attachment://wall.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "uncover", avatar, title="Behind The Wall", color=user.color)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def wanted(self, ctx, user: discord.User = None):
         """Make a wanted poster!"""
 
-        if not user:
-            user = ctx.author
-
+        user = user or ctx.author
         avatar = user.avatar_url_as(format="png")
-
-        async with ctx.typing():
-            async with self.session.get(f"https://api.popcat.xyz/wanted?image={avatar}") as r:
-                embed = discord.Embed(title="WANTED", color=user.color)
-                file = discord.File(BytesIO(await r.read()), filename=f"wanted.png")
-                embed.set_image(url="attachment://wanted.png")
-                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                await ctx.send(embed=embed, file=file)
+        await self.send_embed(ctx, "wanted", avatar, title="WANTED", color=user.color)
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def whowouldwin(self, ctx, user_1: discord.User, user_2: discord.User = None):
         """Who would win?"""
 
-        if not user_2:
-            user_2 = ctx.author
-
+        user_2 = user_2 or ctx.author
+        if user_1 == user_2:
+            return await ctx.send("Of course you would tie against yourself.")
         avatar_1 = user_1.avatar_url_as(format="png")
         avatar_2 = user_2.avatar_url_as(format="png")
+        color = random.choice([user_1.color, user_2.color])
+        async with ctx.typing():
+            async with self.session.get(
+                f"https://api.popcat.xyz/whowouldwin?image1={avatar_1}&image2={avatar_2}"
+            ) as r:
+                embed = discord.Embed(title="Who Would Win?", color=color)
+                file = discord.File(BytesIO(await r.read()), filename="whowouldwin.png")
+                embed.set_image(url="attachment://whowouldwin.png")
+                pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
+                embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
+                await ctx.send(embed=embed, file=file)
 
-        if user_1 == user_2:
-            await ctx.send("Of course you would tie against yourself.")
-        else:
-            async with ctx.typing():
-                async with self.session.get(
-                    f"https://api.popcat.xyz/whowouldwin?image1={avatar_1}&image2={avatar_2}"
-                ) as r:
-                    embed = discord.Embed(title="Who Would Win?", color=await ctx.embed_color())
-                    file = discord.File(BytesIO(await r.read()), filename=f"whowouldwin.png")
-                    embed.set_image(url="attachment://whowouldwin.png")
-                    pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
-                    embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
-                    await ctx.send(embed=embed, file=file)
+    async def generate_image(self, ctx, endpoint: str, avatar: str) -> discord.File:
+        """Generate an image from the user's avatar."""
+        async with ctx.typing():
+            async with self.session.get(
+                f"https://api.popcat.xyz/{endpoint}", params={"image": avatar}
+            ) as r:
+                if r.status != 200:
+                    return await ctx.send("Something went wrong with the API, try again later.")
+                image = await r.read()
+        file = BytesIO(image)
+        return discord.File(file, filename=f"{endpoint}.png")
+
+    async def send_embed(self, ctx, endpoint: str, avatar: str, **kwargs) -> discord.Message:
+        """Send an embed with the generated image."""
+        embed = discord.Embed(**kwargs)
+        pop_cat = "https://c.tenor.com/BT8I5b35oMQAAAAC/oatmeal-meme.gif"
+        embed.set_footer(text="Powered by api.popcat.xyz", icon_url=pop_cat)
+        await ctx.send(embed=embed, file=await self.generate_image(ctx, endpoint, avatar))
