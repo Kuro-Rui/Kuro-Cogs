e3a3b150b64a2d48db945971521d0ee247cb7f9f
Louis Dominic <louisdominic80@gmail.com>
2024-02-01 14:42:55 +0700

    [TypeRacer] Do it properly

--- typeracer/typeracer.py
+++ typeracer/typeracer.py
@@ -101,7 +101,7 @@ class TypeRacer(kuroutils.Cog):
             seconds = (message.created_at - start.created_at).total_seconds()
             wpm = (len(quote) * accuracy / word_length) / (seconds / 60)
             messages.append((message, seconds, accuracy, wpm))
-            message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
+            asyncio.create_task(message.add_reaction("\N{WHITE HEAVY CHECK MARK}"))
             if len(messages) != winners:
                 return False
             return True
